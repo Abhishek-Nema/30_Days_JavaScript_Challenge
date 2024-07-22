27bd18ee0224183575ff64165b52e5683cf8c88b
Abhishek Nema <nemaabhishek3@gmail.com>
2024-07-22 16:53:03 +0530

    push activity_3 file to repo

--- Day_7/activity_3.js
+++ Day_7/activity_3.js
@@ -1,2 +1,17 @@
 
 // task 5
+let library = {
+    name: "Pandey library",
+    books: {
+        book1: "Physics",
+        book2: "Chemistry",
+        book3: "Maths",
+        book4: "GK"
+    }
+}
+console.log(library);
+console.log(library.books);
+console.log(Object.keys(library.books));
+
+// task 6
+console.log(`The name of library is: ${library.name}, and titles of all books are: ${Object.values(library.books).join(", ")}`);
