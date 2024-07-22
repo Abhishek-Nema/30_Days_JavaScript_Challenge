dec73750fdb7458c97ea4736ec2b5065e5e0ef68
Abhishek Nema <nemaabhishek3@gmail.com>
2024-07-23 02:37:10 +0530

    add activity_1 files to repo

---
+++ Day_8/activity_1.js
@@ -0,0 +1,14 @@
+
+// task 1
+let name = "John"
+let age = 30
+
+console.log(`Hi ${name}, as per data you are ${age} years old`);
+
+// task 2
+let str = `mera joota hai japani
+ye patlun englishtaani
+sir pe lal toopi roosi
+phir bhi dil hai hindustani`
+console.log(str);
+
